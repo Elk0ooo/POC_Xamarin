5d39e748cb608f34c568dfe18f8972855850904b
nicolas lebeault <nicolas.lebeault@diiage.org>
2020-12-10 11:18:28 +0100

    POC Xamarin

---
+++ PocDiiageTemplate/PocDiiageTemplate/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PocDiiageTemplate.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _title;
+        public string Title 
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private DelegateCommand _navigateCommand { get; set; }
+        private readonly INavigationService _NavigationService;
+
+        public DelegateCommand NavigateCommand => _navigateCommand ?? (_navigateCommand = new DelegateCommand(ExecuteNavigateCommand));
+      
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Pageeeeeeeee";
+            _NavigationService = navigationService;
+        }
+        private async void ExecuteNavigateCommand()
+        {
+            await _NavigationService.NavigateAsync("Page1");
+        }
+
+    }
+}
---
+++ PocDiiageTemplate/PocDiiageTemplate/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Prism;
+using Prism.Ioc;
+using PocDiiageTemplate.ViewModels;
+using PocDiiageTemplate.Views;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Forms;
+
+namespace PocDiiageTemplate.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
